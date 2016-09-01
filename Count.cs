9c4045083fa8782843945c45e45062182758ea9c
tagashin <synya2486@gmail.com>
2016-09-01 13:39:53 +0900

    結果を表示
    結果を表示、一連の動作を可能にした

--- Count.cs
+++ Count.cs
@@ -4,31 +4,44 @@ using UnityEngine.SceneManagement;
 
 public class Count : MonoBehaviour {
     public int count = 0;
+    private int i = 50;
     private float point = 0;
+    public GameObject A;
     public Vector3 FPOS = new Vector3(0, 0, 0);
     public Vector3 NPOS = new Vector3(0, 0, 0);
     private Vector3 OPOS = new Vector3(0, 0, 0);
 
-	void Start () {}
+	void Start ()
+    {
+        StartCoroutine(counttime());
+        A = GameObject.Find("Camera");
+    }
 	void Update ()
     {
         if(Vector3.Distance(FPOS,OPOS) < Vector3.Distance(NPOS,OPOS))
         {
             FPOS = NPOS;
         }
-        if (Time.time > 30.0f)
+        if(A.transform.position.y < Vector3.Distance(FPOS,OPOS)+50)
         {
-            if(count > 100)
-            {
-                point = 100;
-            }
-            else
-            {
-                point = count;
-            }
-            PlayerPrefs.SetFloat("point",point);
-            //SceneManager.LoadScene();
+            i++;
+            A.transform.position = new Vector3(0,i,0);
         }
 
+
 	}
+    IEnumerator counttime()
+    {
+        yield return new WaitForSeconds(30.0f);
+        if (count > 100)
+        {
+            point = 100;
+        }
+        else
+        {
+            point = count;
+        }
+        PlayerPrefs.SetFloat("point", point);
+        SceneManager.LoadScene("result");
+    }
 }
---
+++ Result.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Result : MonoBehaviour {
+    private float fpoint = 0;
+    public int point = 0;
+    public TextAsset TextA;
+    public string[] getstrings;
+    public string[] cutstrings;
+	void Start () {
+        fpoint = PlayerPrefs.GetFloat("point");
+        point = (int)fpoint;
+        getstrings = TextA.text.Split('\n');
+        if (point == 0) cutstrings = getstrings[0].Split('\t');
+        else if (point > 0 && point <= 10) cutstrings = getstrings[1].Split('\t');
+        else if (point > 10 && point <= 20) cutstrings = getstrings[2].Split('\t');
+        else if (point > 20 && point <= 30) cutstrings = getstrings[3].Split('\t');
+        else if (point > 30 && point <= 40) cutstrings = getstrings[4].Split('\t');
+        else if (point > 40 && point <= 50) cutstrings = getstrings[5].Split('\t');
+        else if (point > 50 && point <= 60) cutstrings = getstrings[6].Split('\t');
+        else if (point > 60 && point <= 70) cutstrings = getstrings[7].Split('\t');
+        else if (point > 70 && point <= 80) cutstrings = getstrings[8].Split('\t');
+        else if (point > 80 && point <= 90) cutstrings = getstrings[9].Split('\t');
+        else if (point > 90 && point <= 100) cutstrings = getstrings[10].Split('\t');
+        StartCoroutine(Show());
+    }
+	
+
+	void Update() { }
+    IEnumerator Show()
+    {
+        yield return new WaitForSeconds(0.0f);
+        GameObject resulttext = GameObject.Find("result");
+        GameObject OVtext = GameObject.Find("OV");
+        GameObject extext = GameObject.Find("ex");
+        resulttext.GetComponent<Text>().text = "漏えい度" + point;
+        OVtext.GetComponent<Text>().text = cutstrings[1];
+        extext.GetComponent<Text>().text = cutstrings[2];
+    }
+}
